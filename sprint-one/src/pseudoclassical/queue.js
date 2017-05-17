3229f16198fa0751b9efb1dbe7caf6f7b180d1c1
Hack Reactor Student <student@hackreactor.com>
2017-05-17 15:58:37 -0700

    implement pseudoclassical stack and queue functions

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype.size = function() {
+  var count = 0;
+  for (let key in this.storage) {
+    count++;
+  }
+  return count;
+};
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.size()] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  let removed = this.storage[0];
+  for (let key in this.storage) {
+    this.storage[key] = this.storage[parseInt(key) + 1];
+  }
+  delete this.storage[this.size() - 1];
+  return removed;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Stack.prototype.size = function() {
+  var count = 0;
+  for (let key in this.storage) {
+    count++;
+  }
+  return count;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.size()] = value;
+};
+
+Stack.prototype.pop = function() {
+  const removed = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return removed;
 };
 
 
