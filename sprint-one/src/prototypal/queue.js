9da3d50062e707d8067dd710971e30d909064410
Hack Reactor Student <student@hackreactor.com>
2017-05-17 15:40:14 -0700

    implement prototypal stack and queue functions

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    let count = 0;
+    for (var key in this.storage) {
+      count++;
+    }
+    return count;
+  },
+  enqueue: function(value) {
+    this.storage[this.size()] = value;
+  },
+  dequeue: function() {
+    const removed = this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key] = this.storage[parseInt(key) + 1];
+    }
+    delete this.storage[this.size() - 1];
+    return removed;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    let count = 0;
+    for (let key in this.storage) {
+      count++;
+    }
+    return count;
+  },
+  push: function(value) {
+    this.storage[this.size()] = value;
+  },
+  pop: function() {
+    const removed = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return removed;  
+  }
+};
 
 
