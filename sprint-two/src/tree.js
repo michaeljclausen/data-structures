b12b6c300209ebbfbb2c46ec5bd86a047a72822a
Hack Reactor Student <student@hackreactor.com>
2017-05-18 10:42:42 -0700

    implement tree functions; answer time complexity for linkedList

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,13 +13,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  let result = false;
+  if (this.value === target) {
+    return true;
+  } else {      
+    this.children.forEach(child => {
+      if (child.contains(target) === true) {
+        result = true;
+      }
+    });
+  } 
+  return result;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .contains is linear.
+ .addChild is constant.
  */
